0f4a1aaf9f5841672cb800cf40c513e2c0b24a4c
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-28 23:51:24 -0600

    delete ckeditor.client.ts plugin

--- src/runtime/ckeditor.client.ts
+++
@@ -1,5 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import CKEditor from './components/CKEditor.vue'
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('Ckeditor', CKEditor)
-})
